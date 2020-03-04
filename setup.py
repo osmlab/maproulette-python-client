1739c4205fdee350111ac819062a41fe6f46f10b
Daniel Baah <dbaah@spatialdev.com>
2020-03-03 22:42:35 -0800

    fix tests

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 
+exclude_dirs = ['examples', 'tests', 'venv']
+
 with open("README.md", "r") as readme:
     long_description = readme.read()
 
@@ -8,7 +10,7 @@ setup(name="maproulette",
       description="A Python API wrapper for MapRoulette",
       long_description=long_description,
       url="https://github.com/osmlab/maproulette-python-client",
-      packages=find_packages(),
+      packages=find_packages('maproulette'),
       python_requires='>=3.6',
       setup_requires=["pytest-runner"],
       tests_require=["pytest"],
