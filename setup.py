a8db74a84dc906ce28b36e829be3ad9aa8a0dc9d
Daniel Baah <danielduhh@gmail.com>
2020-04-03 14:38:55 -0700

    change version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as readme:
     long_description = readme.read()
 
 setup(name="maproulette",
-      version="0.0.1",
+      version="1.0.0-alpha.1",
       description="A Python API wrapper for MapRoulette",
       long_description=long_description,
       url="https://github.com/osmlab/maproulette-python-client",
