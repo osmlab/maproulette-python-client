eb109e2bf13f7ce4c5b97a6c9e8df8438f2b04f7
Matt Manley <matthewmanley113@gmail.com>
2020-04-09 15:13:53 -0700

    Updated examples

--- examples/get_challenge_by_id.py
+++ examples/get_challenge_by_id.py
@@ -2,13 +2,13 @@ import maproulette
 import json
 
 # Create a configuration object for MapRoulette using your API key:
-config = maproulette.Configuration(url='maproulette.org')
+config = maproulette.Configuration(api_key="API_KEY")
 
 # Create an API objects with the above config object:
 api = maproulette.Api(config)
 
 # Specify the ID of the challenge you want up add tasks to:
-challenge_id = 10000000000000000000
+challenge_id = '12974'
 
 # Printing response
 print(json.dumps(api.get_challenge_by_id(challenge_id)))
