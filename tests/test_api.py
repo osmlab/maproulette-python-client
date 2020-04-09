a9ee0dc8741458f88ea162a85e9e49ef95cde325
mattmanley <matthewmanley113@gmail.com>
2020-04-09 11:34:03 -0700

    Add tests for remaining methods (#19)
    * Added tests for existing methods
    
    * Updated tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -11,6 +11,49 @@ class TestAPI(unittest.TestCase):
     api = maproulette.Api(config)
 
     @patch('maproulette.api.maproulette_server.requests.get')
+    def test_get_project_by_id(self, mock_request, api_instance=api):
+        test_project_id = '32922'
+        mock_request.return_value.status_code = '200'
+        response = api_instance.get_project_by_id(test_project_id)
+        self.assertEqual(response['status'], '200')
+
+    @patch('maproulette.api.maproulette_server.requests.get')
+    def test_get_project_by_name(self, mock_request, api_instance=api):
+        test_project_name = 'Maptime!'
+        mock_request.return_value.status_code = '200'
+        response = api_instance.get_project_by_name(test_project_name)
+        self.assertEqual(response['status'], '200')
+
+    @patch('maproulette.api.maproulette_server.requests.get')
+    def test_find_project(self, mock_request, api_instance=api):
+        test_search = 'Health Facilities in India'
+        mock_request.return_value.status_code = '200'
+        response = api_instance.find_project(test_search)
+        self.assertEqual(response['status'], '200')
+
+    @patch('maproulette.api.maproulette_server.requests.get')
+    def test_get_project_children(self, mock_request, api_instance=api):
+        test_project_id = '32922'
+        mock_request.return_value.status_code = '200'
+        response = api_instance.get_project_children(test_project_id)
+        self.assertEqual(response['status'], '200')
+
+    @patch('maproulette.api.maproulette_server.requests.get')
+    def test_get_project_challenges(self, mock_request, api_instance=api):
+        test_project_id = '12974'
+        mock_request.return_value.status_code = '200'
+        response = api_instance.get_project_challenges(test_project_id)
+        self.assertEqual(response['status'], '200')
+
+    @patch('maproulette.api.maproulette_server.requests.post')
+    def test_create_project(self, mock_request, api_instance=api):
+        test_project_model = maproulette.ProjectModel(name='Test_Project_Name',
+                                                      description='This is a test project')
+        mock_request.return_value.status_code = '200'
+        response = api_instance.create_project(test_project_model)
+        self.assertEqual(response['status'], '200')
+
+    @patch('maproulette.api.maproulette_server.requests.get')
     def test_get_task_by_id(self, mock_request, api_instance=api):
         test_task_id = '42914448'
         mock_request.return_value.status_code = '200'
