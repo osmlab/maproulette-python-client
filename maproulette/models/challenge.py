327092d6ed20afc5d34add7a3c7fc05f5b0e6066
Daniel Castro <daniel.castro@critigen.com>
2020-05-15 16:19:35 -0700

    Add new challenge model properties

--- maproulette/models/challenge.py
+++ maproulette/models/challenge.py
@@ -27,6 +27,24 @@ class ChallengeModel:
         self._name = value
 
     @property
+    def created(self):
+        """Time stamp of when the challenge was created"""
+        return self._created
+
+    @created.setter
+    def created(self, value):
+        self._created = value
+
+    @property
+    def modified(self):
+        """Time stamp pf when the challenge was last modified"""
+        return self._modified
+
+    @modified.setter
+    def modified(self, value):
+        self._modified = value
+
+    @property
     def description(self):
         """The description for the challenge"""
         return self._description
@@ -34,6 +52,15 @@ class ChallengeModel:
     @description.setter
     def description(self, value):
         self._description = value
+
+    @property
+    def deleted(self):
+        """Whether a challenge has been deleted or not"""
+        return self._deleted
+
+    @deleted.setter
+    def deleted(self, value):
+        self._deleted = value
 
     @property
     def parent(self):
@@ -99,6 +126,15 @@ class ChallengeModel:
         self._featured = value
 
     @property
+    def virtual_parents(self):
+        """The virtual parents of a challenge"""
+        return self._virtual_parents
+
+    @virtual_parents.setter
+    def virtual_parents(self, value):
+        self._virtual_parents = value
+
+    @property
     def overpassQL(self):
         """The Overpass query for this challenge"""
         return self._overpassQL
@@ -143,12 +179,34 @@ class ChallengeModel:
     def max_zoom(self, value):
         self._max_zoom = value
 
-    def __init__(self, name, id=None, description=None, parent=None, instruction=None, difficulty=None, blurb=None,
-                 enabled=None, challenge_type=None, featured=None, overpassQL=None, default_priority=None, default_zoom=None,
-                 min_zoom=None, max_zoom=None):
+    @property
+    def last_task_refresh(self):
+        """Timestamp of when the challenge's tasks were last refreshed"""
+        return self._last_task_refresh
+
+    @last_task_refresh.setter
+    def last_task_refresh(self, value):
+        self._last_task_refresh = value
+
+    @property
+    def data_origin_date(self):
+        """Timestamp of when data was originated"""
+        return self._data_origin_date
+
+    @data_origin_date.setter
+    def data_origin_date(self, value):
+        self._data_origin_date = value
+
+    def __init__(self, name, id=None, created=None, modified=None, description=None, deleted = None, parent=None,
+                 instruction=None, difficulty=None, blurb=None, enabled=None, challenge_type=None, featured=None,
+                 virtual_parents=None, overpassQL=None, default_priority=None, default_zoom=None, min_zoom=None,
+                 max_zoom=None, last_task_refresh=None, data_origin_date=None):
         self._id = id
         self._name = name
+        self._created = created
+        self._modified = modified
         self._description = description
+        self._deleted = deleted
         self._parent = parent
         self._instruction = instruction
         self._difficulty = difficulty
@@ -156,11 +214,14 @@ class ChallengeModel:
         self._enabled = enabled
         self._challenge_type = challenge_type
         self._featured = featured
+        self._virtual_parents = virtual_parents
         self._overpassQL = overpassQL
         self._default_priority = default_priority
         self._default_zoom = default_zoom
         self._min_zoom = min_zoom
         self._max_zoom = max_zoom
+        self._last_task_refresh = last_task_refresh
+        self._data_origin_date = data_origin_date
 
     def to_dict(self):
         """Converts all non-null properties of a challenge object into a dictionary"""
