c2bd9f3bc9e8559d8c68cfedfd673a53e961e6b1
Matt Manley <matthewmanley113@gmail.com>
2020-05-18 10:14:45 -0700

    Updated docstring

--- maproulette/api/configuration.py
+++ maproulette/api/configuration.py
@@ -1,5 +1,7 @@
 """This module contains the basic configuration object that is used to communicate with the MapRoulette API."""
 
+from typing import Union
+
 DEFAULT_HOSTNAME = 'maproulette.org'
 DEFAULT_PROTOCOL = 'https'
 DEFAULT_API_VERSION = '/api/v2'
@@ -11,17 +13,24 @@ class Configuration:
                  api_key=None, certs=None, verify=True):
         """Create a new configuration object to connect to the MapRoulette server.
 
-        :param str hostname: Optional parameter to specify the hostname of the MapRoulette instance being addressed. Do
+        :param hostname: Optional parameter to specify the hostname of the MapRoulette instance being addressed. Do
             not include the protocol (https, http). Default value is 'maproulette.org'.
-        :param str protocol: Optional parameter to specify the protocol to use for the connection to the MapRoulette
+        :type hostname: str, optional
+        :param protocol: Optional parameter to specify the protocol to use for the connection to the MapRoulette
             instance being addressed such as https or http. Default value is 'https'.
-        :param str api_version: Optional parameter to specify the API version to use. The default is '/api/v2'.
-        :param str api_key: Optional parameter to pass the user-specific API key. This key is necessary for some
-            actions.
-        :param tuple certs: Optional parameter to pass the client-side certificate and key if necessary to make
-            connection with the MapRoulette instance being addressed.
-        :param bool verify: Optional parameter to specify whether to verify SSL certificates for HTTPS requests. Default
+        :type protocol: str, optional
+        :param api_version: Optional parameter to specify the API version to use. The default is '/api/v2'.
+        :type api_version: str, optional
+        :param api_key: Optional parameter to pass the user-specific API key. This key is necessary for some actions.
+        :type api_key: str, optional
+        :param certs: Optional parameter to pass the client-side certificate and key if necessary to make
+            connection with the MapRoulette instance being addressed. Can be either a tuple containing the cert and key
+            file paths (in that order) or a string representing the filepath to both the cert and key stored in a single
+            file.
+        :type certs: str or tuple, optional
+        :param verify: Optional parameter to specify whether to verify SSL certificates for HTTPS requests. Default
             is True.
+        :type verify: bool, optional
         """
         self.api_url = f"{protocol}://{hostname}{api_version}"
         self.base_url = f"{protocol}://{hostname}"
