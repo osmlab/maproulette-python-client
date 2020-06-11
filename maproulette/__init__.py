9eb5c17889ec83175752ea4ae1e8090c711a5295
Matt Manley <matthewmanley113@gmail.com>
2020-06-11 13:47:56 -0700

    Moved imports to single line

--- maproulette/__init__.py
+++ maproulette/__init__.py
@@ -7,8 +7,7 @@ from .api.configuration import Configuration
 from .models.project import ProjectModel
 from .models.challenge import ChallengeModel
 from .models.task import TaskModel
-from .models.priority_rule import PriorityRule
-from .models.priority_rule import PriorityRuleModel
+from .models.priority_rule import PriorityRule, PriorityRuleModel
 from .models import priority_rule
 from .api.project import Project
 from .api.challenge import Challenge
