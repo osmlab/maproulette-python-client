0f144e5754b2f7a74f311746ceaa98a958e0ae1e
Matt Manley <matthewmanley113@gmail.com>
2020-06-16 12:30:14 -0700

    Added task examples

--- examples/task_examples.py
+++ examples/task_examples.py
@@ -7,8 +7,18 @@ config = maproulette.Configuration(api_key="API_KEY")
 # Create an API objects with the above config object:
 api = maproulette.Task(config)
 
-# Specify the ID of the challenge you want up add tasks to:
+# To fetch a task, specify the task ID
 task_id = '42914448'
 
 # Printing response
-print(json.dumps(api.get_task_by_id(task_id)))
+print(json.dumps(api.get_task_by_id(task_id), indent=4, sort_keys=True))
+
+# You can access a task's history:
+print(json.dumps(api.get_task_history(task_id), indent=4, sort_keys=True))
+
+# Or get a task's tags:
+print(json.dumps(api.get_task_tags(task_id), indent=4, sort_keys=True))
+
+# You can also fetch tasks by specifying a list of tags:
+tags = 'tag1,tag2'
+print(json.dumps(api.get_tasks_by_tags(tags), indent=4, sort_keys=True))
