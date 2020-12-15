118ac42e2e778672248a61075217455a5e0fcb76
adam-t-shaw <adam_t_shaw@apple.com>
2020-12-14 18:30:38 -0800

    Moving priority rule enums, cleanup

--- maproulette/models/priority_rule.py
+++ maproulette/models/priority_rule.py
@@ -1,50 +1,7 @@
 """This module contains the definition of a priority rule object in MapRoulette."""
 
 import json
-from enum import Enum, auto
-
-
-class ExtendedEnum(Enum):
-    def _generate_next_value_(name, start, count, last_values):
-        return name.lower()
-
-    @classmethod
-    def list(cls):
-        return [i.value for i in cls]
-
-
-class Conditions(ExtendedEnum):
-    """An enumeration of valid logical conditions for a priority rule object"""
-    OR = "OR"
-    AND = "AND"
-
-
-class Types(ExtendedEnum):
-    """An enumeration of valid types for a priority rule object"""
-    STRING = auto()
-    INTEGER = auto()
-    DOUBLE = auto()
-    LONG = auto()
-
-
-class StringOperators(ExtendedEnum):
-    """An enumeration of valid string operators for a priority rule object"""
-    EQUAL = auto()
-    NOT_EQUAL = auto()
-    CONTAINS = auto()
-    NOT_CONTAINS = auto()
-    IS_EMPTY = auto()
-    IS_NOT_EMPTY = auto()
-
-
-class NumericOperators(ExtendedEnum):
-    """An enumeration of valid numeric operators for a priority rule object"""
-    EQUAL_TO = "=="
-    NOT_EQUAL_TO = "!="
-    LESS_THAN = "<"
-    LESS_THAN_OR_EQUAL = "<="
-    GREATER_THAN = ">"
-    GREATER_THAN_OR_EQUAL = ">="
+from .enums import Conditions, Types, StringOperators, NumericOperators
 
 
 class PriorityRuleModel:
